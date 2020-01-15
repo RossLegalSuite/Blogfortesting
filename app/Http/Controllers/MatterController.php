e082c4d98913ef3818a6f84d0882504f1029c385
Ross Summerell <ross@LSDomain.local>
2020-01-15 09:58:12 +0200

    Commit

---
+++ app/Http/Controllers/MatterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+
+class MatterController extends Controller
+{
+    public function GetMatter(Request $request)
+    {
+
+        $query = DB::connection('odbc-connection-name')
+            ->table('Matter')
+            ->get()
+            ->take(2);
+
+        return $query;
+
+        // $users = DB::select('select top 1 * from Matters');
+
+        // return $users;
+
+        // try {
+
+        //     DB::connection("userdb")->getPdo();
+        //     return 'success';
+        // } catch (\Exception $e) {
+
+        //     die("Could not connect to the database.  Please check your configuration. error:" . $e);
+
+        // }
+
+    }
+
+}
